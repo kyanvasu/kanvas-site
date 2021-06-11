2922296bc30f3dd49f10d18251cd897fc02f5a4c
Juan Daniel <juan@mctekk.com>
2021-06-11 19:51:46 -0400

    feature: desktop styling

--- src/components/HomepageHeader/index.js
+++ src/components/HomepageHeader/index.js
@@ -1,17 +1,28 @@
 import React from 'react';
-import clsx from 'clsx';
+// import clsx from 'clsx';
 import styles from './HomepageHeader.module.scss';
 
 export default function HomepageHeader() {
   return (
-    <header className={clsx(styles.heroBanner)}>
+    <header className={styles.heroBanner}>
       <div className="container">
-        <h1>
-          Bringing back the <span>Love</span> to our daily <span>Dev life</span> one component at a time
-        </h1>
-        <p>
-        Open source smart code development solution, focused on developing SaaS development components so you can focus on the problem at hand.
-        </p>
+        <div className={styles.heroText}>
+          <h1>
+            Bringing back the <span>Love</span> to our daily <span>Dev life</span> one component at a time
+          </h1>
+          <p>
+            Open source smart code development solution, focused on developing SaaS development components so you can focus on the problem at hand.
+          </p>
+        </div>
+
+        <div className={styles.heroImages}>
+          <div className={styles.heroImage1}>
+            <img src={require('@site/static/img/header_1.png').default} />
+          </div>
+          <div className={styles.heroImage2}>
+            <img src={require('@site/static/img/header_2.png').default} />
+          </div>
+        </div>
       </div>
     </header>
   );
